d4343efcbceb61cc0bdadb809f12598e2ef5b3b8
root <root@ip-172-31-20-30.eu-west-1.compute.internal>
2016-07-06 18:30:50 +0000

    upgraded the entrypoint script and added a test script

--- jboss-eap-6.4/resources/jboss.sh
+++ jboss-eap-6.4/resources/jboss.sh
@@ -2,8 +2,16 @@
 
 set -e
 
+# If `docker run` received no additional arguments, run jboss.
 if [[ $# -lt 1 ]]; then
+
+  # Set the default Controller host
+  if [[ $DEFAULT_CONTROLLER_HOST ]]; then
+    sed -i "s#<host>.*#<host>$DEFAULT_CONTROLLER_HOST</host>#" $JBOSS_HOME/bin/jboss-cli.xml
+  fi
+
   $JBOSS_HOME/bin/standalone.sh -c standalone-full-ha.xml $JBOSS_OPTS 
 fi
 
+# Execute whatever argument was passed.
 exec "$@"
