843b551542b134384b3ef927e684265e9519a394
root <root@ip-172-31-20-30.eu-west-1.compute.internal>
2016-07-09 21:28:07 +0000

    Liferay now auto deploys upon container launch

--- jboss-liferay/6.4.0-GA/README.md
+++ jboss-liferay/6.4.0-GA/README.md
@@ -1,3 +1,8 @@
 #### Jboss Version: Jboss Enterprise Application Server 6.4.0 GA
 #### Liferay Version: Liferay Enterprise Edition 6.2 Service Pack 12
 
+>docker-compose -f mysql-compose/default.yml up -d
+>docker-compose up -d
+
+#### To be Updated..
+
--- jboss-liferay/6.4.0-GA/resources/entrypoint.sh
+++ jboss-liferay/6.4.0-GA/resources/entrypoint.sh
@@ -1,22 +1,44 @@
 #!/bin/bash
 
-set -e
+set -e 
 
-FILE=${JBOSS_HOME}/standalone/configuration/standalone.xml
+mkdir -p /opt/jboss/deploy
+# Do not apply automated changes in configuration files if /opt/vars.lock exist.
+# Nice trick!
+if [[ ! -f "/opt/vars.lock" ]]; then
+    # Apply configuration for standalone.xml
+    FILE=${JBOSS_HOME}/standalone/configuration/standalone.xml
+    declare -a JBOSS_AS_ENVS=$(env | grep JBOSS_AS_ | awk -F= '{print $1}')
+    for token in ${JBOSS_AS_ENVS[@]}
+    do
+      replacement=$(env | grep $token | awk -F= '{print $2}')
+      if [[ $(cat $FILE | grep "###$token###" | wc -l) -gt 0 ]]; then
+        echo "Found the token for ${token} replacing with ${replacement} .."        
+        sed -i "s+###$token###+$replacement+g" $FILE
+      fi
+    done
 
-# Apply configuration for standalone.xml
-declare -a LIFERAY_ENVS=$(env | grep LIFERAY | awk -F= '{print $1}')
+    # Apply configuration for portal-ext.properties
+    FILE=/opt/jboss/portal-ext.properties
+    declare -a PORTAL_EXT_ENVS=$(env | grep PORTAL_EXT_ | awk -F= '{print $1}')
+    for token in ${PORTAL_EXT_ENVS[@]}
+    do
+      replacement=$(env | grep $token | awk -F= '{print $2}')
+      if [[ $(cat $FILE | grep "###$token###" | wc -l) -gt 0 ]]; then
+        echo "Found the token for ${token} replacing with ${replacement} .."
+        sed -i "s+###$token###+$replacement+g" $FILE
+      fi
+    done
 
-echo "Replacing tokens in $FILE .."
-for token in ${LIFERAY_ENVS[@]}
-do
-  replacement=$(env | grep $token | awk -F= '{print $2}')
-  echo "${token}: $replacement"
-  sed -i "s+###$token###+$replacement+g" $FILE
-done
+    # Apply changes liferay xml files using the default context root
+    WEB_INF_DIR=$JBOSS_HOME/standalone/deployments/ROOT.war/WEB-INF
+    declare -a FILES=( "$WEB_INF_DIR/jboss-web.xml" "$WEB_INF_DIR/jboss-web.xml.5" )
+    for file in ${FILES[@]}
+    do
+      sed -i "s+<context-root>.*+<context-root>${PORTAL_EXT_CONTEXT_ROOT}</context-root>+" $file
+    done
 
-# Give all ownership back to jboss
-chown -R jboss:jboss /opt/jboss
+fi
 
 # If `docker run` received no additional arguments, then run jboss startup script.
 if [[ $# -lt 1 ]]; then
@@ -25,4 +47,4 @@ fi
 
 # Execute whatever argument was passed like `bash`
 exec "$@"
-
+	
